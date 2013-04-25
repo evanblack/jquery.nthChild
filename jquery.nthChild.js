b303e1d8043c8e7624fc9375c6bd2d228ce395dd
evanblack <evanmblack@gmail.com>
2013-04-25 17:38:54 -0500

    Initial commit

---
+++ jquery.nthChild.js
@@ -0,0 +1,39 @@
+;(function ($) {
+
+	$.fn.nthChild = function(options) {
+
+		/**
+		* cssClasses {String} - The CSS classes to apply to the nth child elements
+		* tags {Array} - The names of tags that are allowed to receive the class name
+		*/
+		var defaults = {cssClasses:['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth'], tags:['div', 'li', 'th', 'tr', 'td']};
+
+		options = $.extend(defaults, options);
+
+		return this.each(function () {
+
+			var $el = $(this),
+				$children = $el.children();
+
+			if($children.length) {
+				var resetVal = $el.data("reset");
+				var resetIndex = 0;
+				$children.each(function(i){
+					if(resetVal){
+						if(resetIndex == resetVal){
+							resetIndex = 0;
+						}
+						$(this).addClass(options.cssClasses[resetIndex]);
+						resetIndex++;
+					} else {
+						$(this).addClass(options.cssClasses[i]);
+					}
+				});
+
+			}
+
+		}); // return
+
+	};
+
+})(jQuery);
